2b417c75998084ee2b9ad2010937858df6a14b19
LexTRND <a.reese@me.com>
2021-06-04 14:20:22 -0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,22 @@ import Hero from "../components/Hero";
 import Featured from "../components/Featured";
 import Skills from "../components/Skills";
 import Portfolio from "../components/Portfolio";
+import Head from "next/head";
+
 
 export default function Home({ projects }) {
   console.log(projects);
   return (
     <div className="main">
+        <Head>
+            <title>Yemil Designs</title>
+            <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta
+                name="description"
+                content="This is a landing page for Alexis Reese. A software developer."
+            />
+            <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
       <Hero />
       <Featured />
       <Skills />
