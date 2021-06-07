f57fd1334a4b2bd4d97d5a4ecfbeba4e474f5437
LexTRND <a.reese@me.com>
2021-06-06 19:35:44 -0700

    Added Page

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,26 +1,36 @@
 import Link from "next/link";
-import React from "react";
+import React, {useState} from "react";
 
 export const NavBar = () => {
+  const [ clicked, setClicked] = useState(false)
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
+  const navClick = () => {
+    setClicked(false);
+  };
   return (
     <div className="nav-container">
       <nav className="navbar">
         <a href="/">
           <img src="/logo.svg" alt="Contact Button" />
         </a>
-        <div className="nav-section">
+        <div className={clicked ? "nav-menu" : "nav-section"}>
           <ul>
             <li>
-              <a href="#portfolio-section">My Work</a>
+              <a href="#portfolio-section" onClick={navClick}>My Work</a>
             </li>
             <li>
-              <a href="#skill-section">My Skills</a>
+              <a href="#skill-section" onClick={navClick}>My Skills</a>
             </li>
             <li>
-              <a href="mailto: a.reese@me.com">Hire Me</a>
+              <a href="mailto: a.reese@me.com" onClick={navClick}>Hire Me</a>
             </li>
           </ul>
+
         </div>
+        <div className="menu" onClick={handleClick}><img src={clicked ? "/close.svg" : "/menu.svg"} alt="menu"/></div>
       </nav>
       <div className="navLinks">
         <ul>
